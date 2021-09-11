dcfa376eee7f587234ace5eb50735a48d6873dd8
kbys <work4kbys@gmail.com>
2021-09-12 01:20:58 +0900

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# mruby-bi-misc
+
+- rand() with Range object
+- coinflip()
+- Bi::crc32()
+- Bi::crc64()
+
+# License
+
+Copyright 2021 kbys <work4kbys@gmail.com>
+
+Apache License Version 2.0
---
+++ mrblib/bi_misc.rb
@@ -0,0 +1,14 @@
+
+alias :_original_rand_ :rand
+def rand(n=nil)
+  if n.is_a? Range
+    return n.min if n.min == n.max
+    n.min + _original_rand_(1+n.max-n.min)
+  else
+    _original_rand_(n)
+  end
+end
+
+def coinflip
+  rand(2) == 0
+end
